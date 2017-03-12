30ba3c71d475f0eda80ab61d5f7ef65ee03a8734
Komarov Nikolay <komarov.nikolay.russia@gmail.com>
2017-03-12 12:47:33 +0300

    Create tcp server and client classes

--- src/main/java/com/remotelauncher/EntryPoint.java
+++ src/main/java/com/remotelauncher/EntryPoint.java
@@ -1,12 +1,33 @@
+/*
+ * REMOTE TASK LAUNCHER
+ *
+ * Developed by Nikolay Komarov and Rostislav Povelikin
+ * UNN, 2017
+ */
 package com.remotelauncher;
 
-/**
- * Created by Nikolay Komarov on 12.03.2017.
- */
+import com.remotelauncher.client.TCPClient;
+import com.remotelauncher.server.TCPServer;
+
 public class EntryPoint {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Thread serverThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                TCPServer tcpServer = new TCPServer();
+                tcpServer.runServer();
+            }
+        });
+        Thread clientThread = new Thread (new Runnable() {
+            @Override
+            public void run() {
+                TCPClient tcpClient = new TCPClient();
+                tcpClient.runClient();
+            }
+        });
+        serverThread.start();
+        clientThread.start();
     }
 
 }
